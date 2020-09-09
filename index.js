bf4fa1b1fc55306badc012b6b8086a469640a346
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-09 18:32:59 +0700

    Add home routes middleware

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 
+const home = require('./routes/home');
+
 app.use(express.json());
+app.use('/', home);
 
 const port = process.env.PORT | 3000;
 app.listen(port, () => { console.log(`Server listening on port ${port}...`) });
