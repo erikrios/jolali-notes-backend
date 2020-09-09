58a556bbbb632e0f2cd706a954f5d5851b9c0526
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-09 18:26:55 +0700

    Add server port

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+
+const port = process.env.PORT | 3000;
+app.listen(port, () => { console.log(`Server listening on port ${port}...`) });
