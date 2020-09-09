0f4ff8d6b1766edc558c456fe01880de5a2a94d5
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-09 18:45:57 +0700

    Add server port

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const home = require('./routes/home');
 app.use(express.json());
 app.use('/', home);
 
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => { console.log(`Server listening on port ${port}...`) });
