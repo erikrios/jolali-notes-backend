d39ecc7d8216ce33ffc19a96494c03ce5d6cc4c9
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-09 19:40:30 +0700

    Clean the code

--- startup/db.js
+++ startup/db.js
@@ -3,6 +3,7 @@ const config = require('config');
 
 module.exports = function () {
     const connectionString = config.get('db');
+    console.log(connectionString);
 
     mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => { console.log('Connected to database...') })
