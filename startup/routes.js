7d8f54e3c33d588ac68ac57cb028f0bfa4fff83e
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-09 19:09:31 +0700

    Clean the code

--- startup/routes.js
+++ startup/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const home = require('./routes/home');
+const home = require('../routes/home');
 
 module.exports = function (app) {
     app.use(express.json());
