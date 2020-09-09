a855f82185741f171cdfa325d5819b876700d998
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-09 19:07:47 +0700

    Add routes middleware

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const app = express();
 
+require('./startup/routes')(app);
 require('./startup/prod')(app);
-
-const home = require('./routes/home');
-
-app.use(express.json());
-app.use('/', home);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => { console.log(`Server listening on port ${port}...`) });
