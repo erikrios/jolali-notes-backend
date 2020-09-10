128855d5738216c07e97317b541a0f9e666dabf5
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-10 10:37:02 +0700

    Import router routes

--- startup/routes.js
+++ startup/routes.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const home = require('../routes/home');
 const notes = require('../routes/notes');
+const users = require('../routes/users');
 
 module.exports = function (app) {
     app.use(express.json());
     app.use('/', home);
     app.use('/api/notes', notes);
+    app.use('/api/users', users);
 }
