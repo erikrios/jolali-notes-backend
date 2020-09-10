3838a1dd1814660759d5e72a6c23d3eee6af8178
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-10 10:54:17 +0700

    Import auth routes

--- startup/routes.js
+++ startup/routes.js
@@ -3,10 +3,12 @@ const express = require('express');
 const home = require('../routes/home');
 const notes = require('../routes/notes');
 const users = require('../routes/users');
+const auth = require('../routes/auth');
 
 module.exports = function (app) {
     app.use(express.json());
     app.use('/', home);
     app.use('/api/notes', notes);
     app.use('/api/users', users);
+    app.use('/api/auth', auth);
 }
