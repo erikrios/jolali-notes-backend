c6edeb2fe696d84ed306d8feb1a71d5c7c17283b
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-12 15:21:15 +0700

    Add api documentation into home routes

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,16 @@
 const config = require('config');
 const express = require('express');
+const marked = require('marked');
+const fs = require('fs');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    const message = config.get('message');
-    return res.send({ message });
+    const path = __dirname + '/../README.md';
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) return res.status(500).send({ error: err.message });
+        res.send(marked(data.toString()));
+    });
 });
 
 module.exports = router;
