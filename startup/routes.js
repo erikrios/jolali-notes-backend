3bc4c2b29e7341373b79f2726c26997c6e08bbae
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-16 13:47:53 +0700

    Import body-parser module, and add urlencoded middleware

--- startup/routes.js
+++ startup/routes.js
@@ -4,9 +4,11 @@ const home = require('../routes/home');
 const notes = require('../routes/notes');
 const users = require('../routes/users');
 const auth = require('../routes/auth');
+const bodyParser = require('body-parser');
 
 module.exports = function (app) {
     app.use(express.json());
+    app.use(bodyParser.urlencoded({extended: true}))
     app.use('/', home);
     app.use('/api/notes', notes);
     app.use('/api/users', users);
