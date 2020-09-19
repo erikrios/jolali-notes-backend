525343a8b3382c7f4ee803828274faeac0cee8e9
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-19 10:53:41 +0700

    Clean the code

--- routes/ads.js
+++ routes/ads.js
@@ -5,8 +5,9 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     const path = __dirname + '/../ads/app-ads.txt';
-    fs.readFile(path, 'utf8', (err, data) => {
-        if (err) return res.status(500).send({error: err.message});
+
+    const data = fs.readFile(path, 'utf8', (err, data) => {
+        if (err) return res.status(500).send({ error: err.message });
         res.send(data);
     });
 });
