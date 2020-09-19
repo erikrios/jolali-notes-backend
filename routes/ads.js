e6c1d622d6ac473c7cae4151224029b4582d71f2
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-19 10:13:20 +0700

    Bugs fixing

--- routes/ads.js
+++ routes/ads.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    const path = __dirname + '../ads/app-ads.txt';
+    const path = __dirname + '/../ads/app-ads.txt';
     fs.readFile(path, 'utf8', (err, data) => {
         if (err) return res.status(500).send({error: err.message});
         res.send(data);
