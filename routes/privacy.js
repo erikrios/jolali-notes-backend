a07887194e0e76c8802ae86606b81a8ad30af7c1
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-19 11:45:59 +0700

    Add privacy policy and terms & condition

--- routes/privacy.js
+++ routes/privacy.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    const path = __dirname + '/../README.md';
+    const path = __dirname + '/../privacy/privacy-policy.md';
     fs.readFile(path, 'utf8', (err, data) => {
         if (err) return res.status(500).send({ error: err.message });
         res.send(marked(data.toString()));
--- startup/routes.js
+++ startup/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const home = require('../routes/home');
 const ads = require('../routes/ads');
+const privacyPolicy = require('../routes/privacy');
 const notes = require('../routes/notes');
 const users = require('../routes/users');
 const auth = require('../routes/auth');
@@ -12,6 +13,7 @@ module.exports = function (app) {
     app.use(bodyParser.urlencoded({ extended: true }))
     app.use('/', home);
     app.use('/app-ads.txt', ads);
+    app.use('privacy', privacyPolicy);
     app.use('/api/notes', notes);
     app.use('/api/users', users);
     app.use('/api/auth', auth);
