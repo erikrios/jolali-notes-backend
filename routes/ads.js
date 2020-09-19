0317dcbb5acc950f74b50da34672a49ede7e619a
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-19 10:09:49 +0700

    Add ads.js"

---
+++ routes/ads.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const fs = require('fs');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    const path = __dirname + '../ads/app-ads.txt';
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) return res.status(500).send({error: err.message});
+        res.send(data);
+    });
+});
+
+module.exports = router;
